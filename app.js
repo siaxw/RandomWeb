fed311760770979b9f25c65c7ce3c42de523f27b
script <slmsttrv1@gmail.com>
2022-06-04 23:22:10 +0500

    js random names

--- app.js
+++ app.js
@@ -30,10 +30,30 @@ let randomGirlsNameArray = girlsName[Math.floor(Math.random() * girlsName.length
 randomGirlsName.innerHTML = randomGirlsNameArray;
 
 let randomBoysName = document.getElementById('randomBoysName');
-let randomBOysNameArray = boysName[Math.floor(Math.random() * boysName.length)]
-randomBoysName.innerHTML = randomBOysNameArray;
+let randomBoysNameArray = boysName[Math.floor(Math.random() * boysName.length)]
+randomBoysName.innerHTML = randomBoysNameArray;
 
 let randomName = document.getElementById('randomName');
 let mixNameArray = girlsName.concat(boysName);
 let randomNameArray = mixNameArray[Math.floor(Math.random() * mixNameArray.length)]
 randomName.innerHTML = randomNameArray;
+
+let girlsButton = document.getElementById('anotherGirlsButton');
+let boysButton = document.getElementById('anotherBoysButton');
+let randomButton = document.getElementById('anotherRandomButton');
+
+girlsButton.addEventListener('click', ()  =>{
+    let randomGirlsNameArray = girlsName[Math.floor(Math.random() * girlsName.length * 0.5)];
+    return randomGirlsName.innerHTML = randomGirlsNameArray;
+})
+
+boysButton.addEventListener('click', () =>{
+    let randomBoysNameArray = boysName[Math.floor(Math.random() * boysName.length * 0.5)]
+    return randomBoysName.innerHTML = randomBoysNameArray;
+} )
+
+randomButton.addEventListener('click', () =>{
+    let mixNameArray = girlsName.concat(boysName);
+    let randomNameArray = mixNameArray[Math.floor(Math.random() * mixNameArray.length * 0.5)]
+    return randomName.innerHTML = randomNameArray;
+})
