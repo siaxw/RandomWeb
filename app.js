f2e4b5a64660e590a43c6c1eaca62f3c64086f23
script <slmsttrv1@gmail.com>
2022-06-05 13:36:37 +0500

    i dont know what im doing

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 import { boysName, girlsName } from './names.js';
 
+//Random Girls name
+
 let randomGirlsName = document.getElementById('randomGirlsName');
 let randomGirlsNameArray = girlsName[Math.floor(Math.random() * girlsName.length)];
 randomGirlsName.innerHTML = randomGirlsNameArray;
 
+//Random Boys Name
 let randomBoysName = document.getElementById('randomBoysName');
 let randomBoysNameArray = boysName[Math.floor(Math.random() * boysName.length)]
 randomBoysName.innerHTML = randomBoysNameArray;
 
+//Random Names
 let randomName = document.getElementById('randomName');
 let mixNameArray = girlsName.concat(boysName);
 let randomNameArray = mixNameArray[Math.floor(Math.random() * mixNameArray.length)]
 randomName.innerHTML = randomNameArray;
 
+// Another Buttons
 let girlsButton = document.getElementById('anotherGirlsButton');
 let boysButton = document.getElementById('anotherBoysButton');
 let randomButton = document.getElementById('anotherRandomButton');
@@ -32,3 +37,5 @@ randomButton.addEventListener('click', () =>{
     let randomNameArray = mixNameArray[Math.floor(Math.random() * mixNameArray.length * 0.5)]
     return randomName.innerHTML = randomNameArray;
 })
+
+// Radio buttons JS
