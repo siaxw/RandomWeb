dec054428b05404bcc03a8424bb9b0b0f7496cad
script <slmsttrv1@gmail.com>
2022-06-22 14:33:07 +0500

    Random Boys Name Btn

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import { boysNames } from "./bnames.js";
 import { girlsNames } from "./gnames.js";
 
+
+// Girls
 const randomGirlsName = document.getElementById('randomNameG');
 const randomGirlsNameBtn = document.getElementById('randomNameGBtn');
 let randomGName = girlsNames[Math.floor(Math.random() * girlsNames.length)];
@@ -12,4 +14,17 @@ randomGirlsName.innerHTML = randomGName;
 randomGirlsNameBtn.addEventListener('click', () =>{
     let randomGName = girlsNames[Math.floor(Math.random() * girlsNames.length)];
     randomGirlsName.innerHTML = randomGName;
+})
+
+
+//Boys
+const randomBoysName = document.getElementById('randomNameB');
+const randomBoysNameBtn = document.getElementById('randomNameBBtn');
+let randomBName = boysNames[Math.floor(Math.random() * boysNames.length)];
+
+randomBoysName.innerHTML = randomBName;
+
+randomBoysNameBtn.addEventListener('click', () =>{
+    let randomBName = boysNames[Math.floor(Math.random() * boysNames.length)];
+    randomBoysName.innerHTML = randomBName;
 })
