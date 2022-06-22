9ecaab9a82c09c24d4b7b03932d1aed992bddf7e
script <slmsttrv1@gmail.com>
2022-06-22 14:22:56 +0500

    Random Girls Name Btn

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 import { boysNames } from "./bnames.js";
 import { girlsNames } from "./gnames.js";
 
-const rGirlsName = document.getElementById('randomGirlsName');
-const rGirlsNameBtn = document.getElementById('girlsAnotherBtn');
+const randomGirlsName = document.getElementById('randomNameG');
+const randomGirlsNameBtn = document.getElementById('randomNameGBtn');
+let randomGName = girlsNames[Math.floor(Math.random() * girlsNames.length)];
 
+console.log(randomGName)
+
+randomGirlsName.innerHTML = randomGName;
+
+randomGirlsNameBtn.addEventListener('click', () =>{
+    let randomGName = girlsNames[Math.floor(Math.random() * girlsNames.length)];
+    randomGirlsName.innerHTML = randomGName;
+})
